2730e3fe0b1294eb408bf2d0aa4710b1d0b3694a
Bhushan <kakadebhushan616@gmail.com>
2024-07-18 23:21:02 +0530

    dynamically added products cards continue..

--- ecommerce-site/homeProducts.js
+++ ecommerce-site/homeProducts.js
@@ -3,9 +3,18 @@ const productsTemplate = document.querySelector(".productsTemplate");
 
 export const showProductContainer = (products) => {
   products.forEach((curproduct) => {
-    const { id, name, star, price, stock, description, image } = curproduct;
+    const { id, name, category, price, stock, image } = curproduct;
 
     const productClone = document.importNode(productsTemplate.content, true);
+
+    productClone.querySelector(".categories").textContent = category;
+    productClone.querySelector(".image").src = image;
+    productClone.querySelector(".name").textContent = name;
+    productClone.querySelector(".price").textContent = `₹${price}`;
+    productClone.querySelector(".actual-price").textContent = `₹${price * 3}`;
+    productClone.querySelector(
+      ".stock"
+    ).textContent = `Total Stocks Available: ${stock}`;
 
     productContainer.append(productClone);
   });
